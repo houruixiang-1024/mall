a37b9dd7f01b1d367d5f1a2f52484ab62bbccc46
Houruixiang-1024 <1252658177@qq.com>
2020-08-08 20:32:19 +0800

    hrx

---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <img :src="goodItem.show.img" alt="">
+  <div>
+    <p>{{goodItem.title}}</p>
+    <span class="price">{{goodItem.price}}</span>
+    <span class="collect">{{goodItem.cfav}}</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "GoodsListItem",
+  props : {
+    goodItem : {
+      type : Object,
+      default(){
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
